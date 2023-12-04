27ffa6ee1d087fc4686e053989c02dec8d2c5b74
lukas.fischer <lukas.fischer@netnode.ch>
2023-12-04 21:29:55 +0100

    added components

--- package/index.ts
+++ package/index.ts
@@ -1,3 +1,6 @@
+export { NodeHiveClient } from './src/NodeHiveClient';
+
+
 export { AuthWrapper } from './src/components/auth/AuthWrapper';
 export { DevHelper } from './src/components/helper/DevHelper';
 export { LoginForm } from './src/components/auth/LoginForm';
@@ -8,3 +11,4 @@ export { NodeMeta } from './src/components/node/NodeMeta';
 
 export { VisualEditorNodeWrapper } from './src/components/visualeditor/VisualEditorNodeWrapper';
 export { VisualEditorParagraphWrapper } from './src/components/visualeditor/VisualEditorParagraphWrapper';
+
