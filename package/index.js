ae42e1460d0a0ac85e2fcc26324c46de5a8c69e6
Yannick Bättig <yannick.baettig@netnode.ch>
2025-11-01 19:18:04 +0100

    Fix import

--- package/index.js
+++ package/index.js
@@ -7,14 +7,14 @@ import {
   ValidationError,
   ConfigurationError,
 } from "./src/errors.js";
-import { AuthManager } from "./auth/AuthManager";
-import { MemoryStorage } from "./auth/storage/MemoryStorage.js";
-import { BrowserStorage } from "./auth/storage/BrowserStorage.js";
-import { CookieStorage } from "./auth/storage/CookieStorage.js";
-import { ApiKeyStrategy } from "./auth/strategies/ApiKeyStrategy.js";
-import { JwtStrategy } from "./auth/strategies/JwtStrategy.js";
-import { OAuthClientCredentialsStrategy } from "./auth/strategies/OAuthClientCredentialsStrategy.js";
-import { OAuthPasswordStrategy } from "./auth/strategies/OAuthPasswordStrategy.js";
+import { AuthManager } from "./src/auth/AuthManager.js";
+import { MemoryStorage } from "./src/auth/storage/MemoryStorage.js";
+import { BrowserStorage } from "./src/auth/storage/BrowserStorage.js";
+import { CookieStorage } from "./src/auth/storage/CookieStorage.js";
+import { ApiKeyStrategy } from "./src/auth/strategies/ApiKeyStrategy.js";
+import { JwtStrategy } from "./src/auth/strategies/JwtStrategy.js";
+import { OAuthClientCredentialsStrategy } from "./src/auth/strategies/OAuthClientCredentialsStrategy.js";
+import { OAuthPasswordStrategy } from "./src/auth/strategies/OAuthPasswordStrategy.js";
 
 export {
   NodeHiveClient,
